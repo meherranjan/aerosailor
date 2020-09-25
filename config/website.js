1dc547064c3ca7b8f9261f8a6671c0338ff02592
Meher Ranjan Howji <hmeherranjan@gmail.com>
2020-09-25 10:45:49 +0530

    updated website config

--- config/website.js
+++ config/website.js
@@ -1,9 +1,9 @@
 
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Aerosailor — JS & Everything Else', // Navigation and Site Title
+  title: 'Aerosailor — JavaScript, Technology, Music & everything under the sun', // Navigation and Site Title
   titleAlt: 'Aerosailor', // Title for JSONLD
-  description: 'A blog featuring content on JavaScript, ReactJS, Functional Programming, Elm, Machine Learning, Technology, Web, Scrum, Book Reviews & Lifestyle.',
+  description: 'A blog by Meher featuring content on JavaScript, ReactJS, Functional Programming, Elm, Machine Learning, Technology, Web, Scrum, Book Reviews & Lifestyle.',
   headline: 'Writing and publishing content from Meher', // Headline for schema.org JSONLD
   url: 'https://aerosailor.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
@@ -13,7 +13,7 @@ module.exports = {
   // JSONLD / Manifest
   favicon: 'src/favicon.png', // Used for manifest favicon generation
   shortName: 'Aerosailor', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'meherranjan', // Author for schemaORGJSONLD
+  author: 'Meher Ranjan Howji', // Author for schemaORGJSONLD
   themeColor: '#3D63AE',
   backgroundColor: '#FFF',
 

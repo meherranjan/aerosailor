a5b770d219ab9781ebb3e744bc6ee82dd5250ed8
Meher Ranjan Howji <hmeherranjan@gmail.com>
2021-04-30 13:34:12 +0530

    updated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				// icon: `src/images/gatsby-icon.png`,
+				icon: `./static/icons/icon-512x512.png`,
 			},
 		},
 		`gatsby-plugin-offline`,
